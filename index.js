b41db46be00674d34c4a10892fef79be743acbfd
York Rushin <rushinyork@gmail.com>
2022-06-26 21:17:55 -0700

    commit tree 94842acc1fa14ccbcaf922fdde5b59deec46b922

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+
+function prependCat(name) {
+    const coolCats = [...cats];
+    coolCats.unshift(name);
+    return coolCats;
+}
+
+function removeLastCat() {
+    const goodCats = [...cats];
+    goodCats.pop();
+    return goodCats;
+}
+
+function removeFirstCat() {
+    const badCats = [...cats];
+    badCats.shift();
+    return badCats;
+}
